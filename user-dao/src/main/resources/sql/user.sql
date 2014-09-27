fbfd953270990c0e5dffcb6faa0431a8ae0017b9
p0po <w-y-p-x-l@163.com>
2014-09-27 21:57:37 +0800

    添加初始化数据库脚本

---
+++ user-dao/src/main/resources/sql/user.sql
@@ -0,0 +1,9 @@
+﻿DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=128 DEFAULT CHARSET=utf8;
