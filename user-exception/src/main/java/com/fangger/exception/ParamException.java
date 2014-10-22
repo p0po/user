fc693cd23db1614cdb4e5294d065878526433ab4
p0po <w-y-p-x-l@163.com>
2014-10-22 23:25:15 +0800

    异常

---
+++ user-exception/src/main/java/com/fangger/exception/ParamException.java
@@ -0,0 +1,10 @@
+package com.fangger.exception;
+
+/**
+ * Created by popo on 2014/10/6.
+ */
+public class ParamException extends Exception{
+    public ParamException(String message) {
+        super(message);
+    }
+}
