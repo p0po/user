a2dfdab1c1f3f60bc94212097dad8d2d63296261
p0po <w-y-p-x-l@163.com>
2014-10-22 23:25:38 +0800

    用户service

--- user-service/src/main/java/com/fangger/service/UserService.java
+++ user-service/src/main/java/com/fangger/service/UserService.java
@@ -3,7 +3,18 @@ package com.fangger.service;
 import java.util.List;
 
 import com.fangger.dao.mysql.model.User;
+import com.fangger.exception.UserException;
 
 public interface UserService {
 	public List<User> getAllUser();
+    public int addUser(User user) throws UserException;
+    public void editUser(User user) throws UserException;
+    public void delUser(int id);
+    public User getUser(int id);
+    public User getUserByAccount(String account);
+    public User login(String account,String password);
+    public boolean checkAccount(String account);
+    public void resetPassword(User user) throws UserException;
+    public int getTotalNum();
+    public List<User> getUserByPage(int start,int limit);
 }
--- user-service/src/main/java/com/fangger/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/fangger/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.fangger.service.impl;
 import java.util.Collections;
 import java.util.List;
 
+import com.fangger.exception.UserException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.fangger.dao.mysql.mapper.UserMapper;
@@ -10,6 +11,7 @@ import com.fangger.dao.mysql.model.User;
 import com.fangger.dao.mysql.model.UserExample;
 import com.fangger.service.UserService;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -24,5 +26,107 @@ public class UserServiceImpl implements UserService{
 		}
 		return Collections.emptyList();
 	}
-	
+
+    @Override
+    public int addUser(User user) throws UserException {
+        if(user == null){
+            throw  new UserException("无用户");
+        }
+
+        if (StringUtils.isEmpty(user.getAccount())){
+            throw  new UserException("用户名不能为空");
+        }
+
+        if (StringUtils.isEmpty(user.getPassword())){
+            throw  new UserException("密码不能为空");
+        }
+
+        if(checkAccount(user.getAccount())){
+            throw new UserException("用户："+user.getAccount()+" 已经存在。");
+        }
+
+        user.setPassword(generatePassword(user.getPassword()));
+
+        return userMapper.insert(user);
+    }
+
+    private String generatePassword(String pwd){
+
+        return pwd;
+    }
+
+    @Override
+    public void editUser(User user) throws UserException {
+        if(user == null){
+            throw  new UserException("无用户");
+        }
+
+        if(checkAccount(user.getAccount())){
+            throw new UserException("用户："+user.getAccount()+" 已经存在。");
+        }
+
+        user.setPassword(null);
+
+        userMapper.updateByPrimaryKeySelective(user);
+    }
+
+    @Override
+    public void delUser(int id) {
+        userMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public User getUser(int id) {
+        return userMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public User getUserByAccount(String account) {
+        UserExample example = new UserExample();
+        example.createCriteria().andAccountEqualTo(account);
+        List<User> userList = userMapper.selectByExample(example);
+        return userList.size()==0?null:userList.get(0);
+    }
+
+    @Override
+    public User login(String account, String password) {
+        UserExample example = new UserExample();
+        example.createCriteria()
+                .andAccountEqualTo(account)
+                .andPasswordEqualTo(password);
+        List<User> userList = userMapper.selectByExample(example);
+        return userList.size()==0?null:userList.get(0);
+    }
+
+    @Override
+    public boolean checkAccount(String account) {
+        return getUserByAccount(account) == null?false:true;
+    }
+
+    @Override
+    public void resetPassword(User user) throws UserException {
+        if(user == null){
+            throw  new UserException("无用户");
+        }
+        user.setAccount(null);
+        user.setPassword(generatePassword(user.getPassword()));
+        userMapper.updateByPrimaryKeySelective(user);
+    }
+
+    @Override
+    public int getTotalNum() {
+        UserExample example = new UserExample();
+        example.createCriteria().isValid();
+        return userMapper.countByExample(example);
+    }
+
+    @Override
+    public List<User> getUserByPage(int start, int limit) {
+        UserExample example = new UserExample();
+
+        return null;
+    }
+
+
 }
+
