5b040b9e39e05743c62f5755e1be6d3c30e41b20
wangyongpo@36kr.com <wangyongpo@36kr.com>
2015-01-09 22:40:02 +0800

    增加rest测试

---
+++ user-web/src/main/java/com/fangger/controllers/RestDemoController.java
@@ -0,0 +1,30 @@
+package com.fangger.controllers;
+
+import com.fangger.model.JsonResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by p0po on 15/1/9.
+ */
+@RestController
+@RequestMapping("/demo1")
+public class RestDemoController {
+    @RequestMapping(value="/jsonString3",method = RequestMethod.GET)
+    @ResponseBody
+    public Object map3(){
+        String result = "hello：王永珀";
+        Map<String,String> map = new HashMap<String,String>();
+        map.put("1","english");
+        map.put("2","中文");
+        map.put("3",null);
+        return JsonResponse.ok(map);
+    }
+
+}
