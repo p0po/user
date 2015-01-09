dfa5276d66d83f3fb1e398893e52c7bac3bf2a2e
wangyongpo@36kr.com <wangyongpo@36kr.com>
2015-01-09 22:46:17 +0800

    修改json返回值key名称

--- user-web/src/main/java/com/fangger/model/JsonResponse.java
+++ user-web/src/main/java/com/fangger/model/JsonResponse.java
@@ -7,7 +7,7 @@ import com.alibaba.fastjson.JSON;
  */
 public class JsonResponse {
     int code = 0;
-    Object content;
+    Object data;
     public static JsonResponse ok(){
         return new JsonResponse(0,"");
     }
@@ -20,8 +20,8 @@ public class JsonResponse {
     public static JsonResponse bad(Object content){
         return new JsonResponse(1,content);
     }
-    public static JsonResponse other(int code,Object content){
-        return new JsonResponse(code,content);
+    public static JsonResponse other(int code,Object data){
+        return new JsonResponse(code,data);
     }
 
     @Override
@@ -37,17 +37,17 @@ public class JsonResponse {
         this.code = code;
     }
 
-    public Object getContent() {
-        return content;
+    public Object getData() {
+        return data;
     }
 
-    public void setContent(Object content) {
-        this.content = content;
+    public void setData(Object data) {
+        this.data = data;
     }
 
-    private JsonResponse(int code,Object content){
+    private JsonResponse(int code,Object data){
             this.code = code;
-            this.content = content;
+            this.data = data;
     }
 
 }
