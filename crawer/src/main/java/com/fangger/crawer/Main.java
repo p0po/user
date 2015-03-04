da2adcdec6b4f0619d347de92c39a99f52c2b991
p0po <w-y-p-x-l@163.com>
2015-03-04 22:47:55 +0800

    dev test

---
+++ crawer/src/main/java/com/fangger/crawer/Main.java
@@ -0,0 +1,25 @@
+package com.fangger.crawer;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.fangger.utils.httpclient.GetClient;
+
+/**
+ * Created by p0po on 15-2-24.
+ */
+public class Main {
+    int[] city = {110000};
+    int[] district = {1,2,3,4,5,8,9,10,11,12,14,13,15,16,17};
+    public static void main(String[] args) {
+        String result = GetClient.getWithPool("http://m.lianjia.com/wap/serverapi/communityByCondition?type=0&cityId=110000&districtId=2&start=0&rows=300");
+
+        JSONObject jsonObject = (JSONObject)JSON.parse(result);
+
+        int total = jsonObject.getInteger("numFound");
+        int start = jsonObject.getInteger("start");
+        JSONArray jsonArray = jsonObject.getJSONArray("docs");
+
+        System.out.println(jsonArray.size()+"  "+total);
+    }
+}
--- user-web/src/main/java/com/fangger/model/JsonResponse.java
+++ user-web/src/main/java/com/fangger/model/JsonResponse.java
@@ -1,6 +1,12 @@
 package com.fangger.model;
 
 import com.alibaba.fastjson.JSON;
+import com.fangger.utils.bean.BeanToMapUtil;
+
+import java.beans.IntrospectionException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Collection;
+import java.util.Map;
 
 /**
  * Created by popo on 2014/10/3.
@@ -8,25 +14,107 @@ import com.alibaba.fastjson.JSON;
 public class JsonResponse {
     int code = 0;
     Object data;
+    String msg = "";
+
+    private static final String SUCCESS = "success";
+    private static final String FAILURE = "failed";
+
     public static JsonResponse ok(){
-        return new JsonResponse(0,"");
+        return new JsonResponse(0,SUCCESS,null);
     }
     public static JsonResponse ok(Object content){
-        return new JsonResponse(0,content);
+        return new JsonResponse(0,SUCCESS,content);
     }
-    public static JsonResponse bad(){
-        return new JsonResponse(1,"");
-    }
-    public static JsonResponse bad(Object content){
-        return new JsonResponse(1,content);
-    }
-    public static JsonResponse other(int code,Object data){
-        return new JsonResponse(code,data);
+    public static JsonResponse ok(Object content,boolean toAddMore){
+        if (content instanceof Collection) {
+            throw new IllegalArgumentException("不支持Collection类型参数，请用ok(content)方法");
+        }
+
+        if (content instanceof Number){
+            throw new IllegalArgumentException("不支持Number类型参数，请用ok(content)方法");
+        }
+
+        if(toAddMore){
+            try {
+                return new JsonResponse(0,SUCCESS, BeanToMapUtil.objectGetToMap(content));
+            } catch (IntrospectionException e) {
+                e.printStackTrace();
+                throw new IllegalArgumentException("转化失败");
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+                throw new IllegalArgumentException("转化失败");
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+                throw new IllegalArgumentException("转化失败");
+            }
+        }else {
+            return ok(content);
+        }
     }
 
-    @Override
-    public String toString() {
-        return JSON.toJSONString(this);
+    public static JsonResponse bad(){
+        return new JsonResponse(1,FAILURE,null);
+    }
+    public static JsonResponse bad(Object content){
+        return new JsonResponse(1,FAILURE,content);
+    }
+    public static JsonResponse bad(Object content,boolean toAddMore){
+        if (content instanceof Collection) {
+            throw new IllegalArgumentException("不支持Collection类型参数，请用ok(content)方法");
+        }
+
+        if (content instanceof Number){
+            throw new IllegalArgumentException("不支持Number类型参数，请用ok(content)方法");
+        }
+
+        if(toAddMore){
+            try {
+                return new JsonResponse(0,SUCCESS,BeanToMapUtil.objectGetToMap(content));
+            } catch (IntrospectionException e) {
+                e.printStackTrace();
+                throw new IllegalArgumentException("转化失败");
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+                throw new IllegalArgumentException("转化失败");
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+                throw new IllegalArgumentException("转化失败");
+            }
+        }else {
+            return ok(content);
+        }
+    }
+
+    /**
+     * 合并属性字段到对象
+     * @param key
+     * @param obj
+     * @return
+     * @throws IllegalArgumentException
+     */
+    public JsonResponse merge(String key,Object obj) throws IllegalArgumentException{
+        if(this.data == null||!(this.data instanceof Map)){
+            throw new IllegalArgumentException("请先用ok(content,true)实例化再调用该方法");
+        }
+        ((Map) this.data).put(key,obj);
+        return this;
+    }
+
+    /**
+     * 0和1状态不能覆盖，不信你试试
+     * @param code
+     * @param msg
+     * @param content
+     * @return
+     */
+    public static JsonResponse other(int code,String msg,Object content){
+        if(code == 0){
+            msg = SUCCESS;
+        }
+        if(code == 1){
+            msg = FAILURE;
+        }
+        return new JsonResponse(code,msg,content);
     }
 
     public int getCode() {
@@ -45,9 +133,24 @@ public class JsonResponse {
         this.data = data;
     }
 
-    private JsonResponse(int code,Object data){
-            this.code = code;
-            this.data = data;
+    public String getMsg() {
+        return msg;
     }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    private JsonResponse(int code,String msg,Object data){
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return JSON.toJSONString(this);
+    }
+
 
 }
