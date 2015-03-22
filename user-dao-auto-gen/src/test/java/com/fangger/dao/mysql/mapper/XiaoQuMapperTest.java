8d03f1cf60ac08884ff7f47c1e098eb3b0caab5c
p0po <w-y-p-x-l@163.com>
2015-03-22 23:10:12 +0800

    test

--- user-dao-auto-gen/src/test/java/com/fangger/dao/mysql/mapper/XiaoQuMapperTest.java
+++ user-dao-auto-gen/src/test/java/com/fangger/dao/mysql/mapper/XiaoQuMapperTest.java
@@ -1,7 +1,31 @@
 package com.fangger.dao.mysql.mapper;
 
+import com.fangger.mapper.XiaoQuMapper;
+import com.fangger.model.XiaoQu;
+import com.fangger.model.XiaoQuCondition;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
 /**
  * Created by popo on 2014/10/26.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext-dataSource.xml"})
 public class XiaoQuMapperTest {
+    @Autowired
+    XiaoQuMapper xiaoQuMapper;
+
+    @Test
+    public void testCondition(){
+        XiaoQuCondition xiaoQuCondition = new XiaoQuCondition();
+        xiaoQuCondition.createCriteria().andIdIsNotNull();
+        List<XiaoQu> xiaoQuList = xiaoQuMapper.selectByCondition(xiaoQuCondition);
+        Assert.assertTrue(xiaoQuList.size()>0);
+    }
 }
