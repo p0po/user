ed7f3535765d6c234fb2fd1faa4e3dbe9a69c97e
p0po <w-y-p-x-l@163.com>
2015-03-22 23:09:50 +0800

    maptree

--- user-controller/src/main/java/com/fangger/controller/user/MapTreeController.java
+++ user-controller/src/main/java/com/fangger/controller/user/MapTreeController.java
@@ -1,7 +1,82 @@
 package com.fangger.controller.user;
 
+import org.apache.commons.collections.SetUtils;
+import org.springframework.beans.factory.BeanFactoryUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
+import org.springframework.web.servlet.mvc.condition.RequestMethodsRequestCondition;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by p0po on 15-3-19.
  */
+@RestController
+@RequestMapping("info")
 public class MapTreeController {
+    @RequestMapping(value = "tree", method = RequestMethod.GET)
+    public Object getRequestTree(HttpServletRequest request) {
+        ServletContext servletContext = request.getSession().getServletContext();
+        WebApplicationContext appContext = WebApplicationContextUtils.getWebApplicationContext(servletContext);
+
+        Map<String, HandlerMapping> allRequestMappings = BeanFactoryUtils.beansOfTypeIncludingAncestors(appContext,
+                HandlerMapping.class, true, false);
+
+        for (HandlerMapping handlerMapping : allRequestMappings.values()){
+            if (handlerMapping instanceof RequestMappingHandlerMapping){
+                RequestMappingHandlerMapping requestMappingHandlerMapping = (RequestMappingHandlerMapping) handlerMapping;
+                Map<RequestMappingInfo, HandlerMethod> handlerMethods = requestMappingHandlerMapping.getHandlerMethods();
+                return handlerMapping;
+            }
+        }
+
+        return allRequestMappings;
+    }
+
+    private final RequestMappingHandlerMapping handlerMapping;
+
+    @Autowired
+    public MapTreeController(RequestMappingHandlerMapping handlerMapping) {
+        this.handlerMapping = handlerMapping;
+    }
+
+    @RequestMapping(value="/endpointdoc", method=RequestMethod.GET)
+    public Object show() {
+        List<RequestMappingInfo> list = new ArrayList<>();
+        Map<RequestMappingInfo, HandlerMethod> handlerMethods = this.handlerMapping.getHandlerMethods();
+        for (Map.Entry<RequestMappingInfo, HandlerMethod> requestMappingInfoHandlerMethodEntry : handlerMethods.entrySet())
+        {
+            RequestMappingInfo requestMappingInfo = requestMappingInfoHandlerMethodEntry.getKey();
+            HandlerMethod mappingInfoValue = requestMappingInfoHandlerMethodEntry.getValue();
+
+            RequestMethodsRequestCondition methodCondition = requestMappingInfo.getMethodsCondition();
+            Set<RequestMethod> methods = methodCondition.getMethods();
+
+            PatternsRequestCondition patternsCondition = requestMappingInfo.getPatternsCondition();
+            Set<String> patterns = patternsCondition.getPatterns();
+
+            String controllerName = mappingInfoValue.getBeanType().toString();
+            String requestMethodName = mappingInfoValue.getMethod().getName();
+            Class<?>[] methodParamTypes = mappingInfoValue.getMethod().getParameterTypes();
+            list.add(requestMappingInfo);
+        }
+        return list;
+    }
+
 }
