b96cd83fa50458757819843548132c75564b4cb3
p0po <w-y-p-x-l@163.com>
2015-03-22 23:02:33 +0800

    test

---
+++ user-common/src/test/java/com/fangger/utils/bean/BeanUtilTest.java
@@ -0,0 +1,45 @@
+package com.fangger.utils.bean;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* BeanUtil Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>三月 21, 2015</pre> 
+* @version 1.0 
+*/ 
+public class BeanUtilTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: objectFildToMap(Object o) 
+* 
+*/ 
+@Test
+public void testObjectFildToMap() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: objectGetToMap(Object obj) 
+* 
+*/ 
+@Test
+public void testObjectGetToMap() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
