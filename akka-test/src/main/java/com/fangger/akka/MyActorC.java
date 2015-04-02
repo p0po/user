24bfaaf3c26bb2970af0ac721574428a8467d587
p0po <w-y-p-x-l@163.com>
2015-04-02 21:33:58 +0800

    akka学习

--- akka-test/src/main/java/com/fangger/akka/MyActorC.java
+++ akka-test/src/main/java/com/fangger/akka/MyActorC.java
@@ -1,7 +1,20 @@
 package com.fangger.akka;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
 /**
  * Created by p0po on 15-4-1.
  */
-public class MyActorC {
+public class MyActorC  {
+    public static void main(String[] args) {
+        ActorSystem rootSystem = ActorSystem.create("p0po");
+        ActorRef actorRef = rootSystem.actorOf(Props.create(MyUntypedActor.class),"actor");
+
+        actorRef.tell(12312434,null);
+        for(int i=0;i<10;i++)
+        actorRef.tell("dddd-"+i,null);
+        actorRef.tell(1122,null);
+    }
 }
