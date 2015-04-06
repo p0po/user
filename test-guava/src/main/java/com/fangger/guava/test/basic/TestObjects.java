e8fabea8b1d5f0a8e2590fd4477527d7a3da65ef
p0po <w-y-p-x-l@163.com>
2015-04-06 21:46:54 +0800

    test

--- test-guava/src/main/java/com/fangger/guava/test/basic/TestObjects.java
+++ test-guava/src/main/java/com/fangger/guava/test/basic/TestObjects.java
@@ -1,7 +1,16 @@
 package com.fangger.guava.test.basic;
 
+import com.google.common.base.Objects;
+
 /**
  * Created by p0po on 15-4-3.
  */
 public class TestObjects {
+    public static void main(String[] args) {
+        boolean isEqa = Objects.equal(null,new Object());
+        System.out.println("isEqa = [" + isEqa + "]");
+
+        int hashCode = Objects.hashCode(new O1());
+        System.out.println("hashCode = [" + hashCode + "]");
+    }
 }
--- test-guava/src/main/java/com/fangger/guava/test/basic/TestOrding.java
+++ test-guava/src/main/java/com/fangger/guava/test/basic/TestOrding.java
@@ -1,7 +1,21 @@
 package com.fangger.guava.test.basic;
 
+import com.google.common.collect.Ordering;
+import org.springframework.beans.BeanUtils;
+
 /**
  * Created by p0po on 15-4-3.
  */
 public class TestOrding {
+    public static void main(String[] args) {
+        Ordering<O1> o1Ordering = new Ordering<O1>() {
+            @Override
+            public int compare(O1 o1, O1 t1) {
+                return 0;
+            }
+        };
+
+        Ordering<Comparable> natural = Ordering.natural();
+        Ordering<Comparable> comparableOrdering = natural.nullsFirst();
+    }
 }
